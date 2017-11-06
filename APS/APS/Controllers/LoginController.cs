2866c8474e4ff461f086708faf2c1c1cf70322c7
bbarreto08 <bruno-cb2011@live.com>
2017-11-06 09:32:54 -0200

    Estrutura do login e autenticação feita pelo WS de login.

--- APS/APS/Controllers/LoginController.cs
+++ APS/APS/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using APS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace APS.Controllers
 {
@@ -11,6 +14,49 @@ namespace APS.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string login, string senha)
+        {
+            HttpClient client;            
+            Usuario usuario;
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("aplication/json"));
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("aps/api/login?usuario=" + login + "&senha=" + senha + "&tipoLogin=2").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    usuario = JsonConvert.DeserializeObject<Usuario>(response.Content.ReadAsStringAsync().Result);
+
+                    if (usuario.ErroMsg.Equals(""))
+                    {
+
+                        
+                    } else
+                    {
+                        ViewBag.MsgErro = "Falha na autenticação. Erro: " + usuario.ErroMsg;
+                    }
+
+                } else
+                {
+                    ViewBag.MsgErro = "Falha na comunicação. Erro: " + (int) response.StatusCode;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+                 
+
             return View();
         }
     }
